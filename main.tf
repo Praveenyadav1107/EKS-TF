3f2adb94b9bed23c7f0f1b50e5ab5fa89799ae88
Praveen yadav <praveenyadav1107@gmail.com>
2026-09-01 17:01:31 +0530

    Adjust scaling configuration for instance group

--- main.tf
+++ main.tf
@@ -305,8 +305,8 @@ resource "aws_eks_node_group" "main" {
   instance_types  = ["t3.medium"]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 5
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
